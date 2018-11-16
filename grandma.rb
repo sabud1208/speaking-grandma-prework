cdb606a6b436455cff1a7fc0d951b80d953ac7cd
Stephanie Messad <sabud1208@gmail.com>
2018-11-16 06:37:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,11 +3,3 @@ def speak_to_grandma
   if I love you == true
     return "I LOVE YOU TOO PUMPKIN!"
 end
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so)
-# and yells back
-
-# NO, NOT SINCE 1938!
